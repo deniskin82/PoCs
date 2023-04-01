235c91659298a21d8f9525d0aeec46824383eb9e
deniskin82 <386949+deniskin82@users.noreply.github.com>
2023-03-31 23:11:29 -0400

    Update hashitools_install.sh
    install latest or pick a version you want

--- opscli/hashitools_install.sh
+++ opscli/hashitools_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HASHI_APP=${1:-packer}
+APP_VERSION=${2:-latest}
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 MACHINE=$(uname -m)
 if [ "$MACHINE" == "x86_64" ]; then
@@ -11,7 +12,7 @@ else
     echo "Unknown machine architecture '$MACHINE'" >&2
     exit 1
 fi
-HASHI_URL=$(curl -s "https://api.releases.hashicorp.com/v1/releases/${HASHI_APP}/latest" | jq -Mr ".builds[] | select(.arch == \"${ARCH}\" and .os == \"${OS}\") | .url")
+HASHI_URL=$(curl -s "https://api.releases.hashicorp.com/v1/releases/${HASHI_APP}/${APP_VERSION}" | jq -Mr ".builds[] | select(.arch == \"${ARCH}\" and .os == \"${OS}\") | .url")
 
 curl -s ${HASHI_URL} -o "/tmp/${HASHI_APP}.zip" \
     && unzip -q -d /tmp/ /tmp/${HASHI_APP}.zip \
